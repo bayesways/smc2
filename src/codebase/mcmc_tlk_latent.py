corrected bug in weight calculation

src/codebase/mcmc_tlk_latent.py
@@ -39,9 +39,8 @@ def get_latent_weights_1(
         for t in range(data['N']):
             weights[m,t] = bernoulli.logpmf(
                 data['D'][t],
-                p=expit(latent_particles['y_latent'][m])
+                p=expit(latent_particles['y_latent'][m,t])
             ).sum()
-    # return np.mean(weights,axis=1)
     return weights
 
 

src/run_mcmc.py
@@ -55,7 +55,7 @@ else:
 
 particles.sample_prior_particles(exp_data.get_stan_data())
 
-nsim_mcmc = 100
+nsim_mcmc = 500
 betas = np.empty((nsim_mcmc, 6, 1))
 alphas = np.empty((nsim_mcmc, 6))
 zs = np.empty((nsim_mcmc, data_sim, 1))
@@ -80,4 +80,5 @@ ps['alpha'] = alphas
 ps['beta'] = betas
 ps['z'] = zs
 ps['y'] = ys
+ps['accs'] = particles.acceptance
 save_obj(ps, 'mcmc_post_samples', log_dir)