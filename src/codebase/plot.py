5436352aba2ba5285d6574472714fb68e6b63ff9
konstantinos <kv@itemgmt.com>
2020-12-18 21:35:51 -0500

    added simulation for AZ

---
+++ src/codebase/plot.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import altair as alt
+alt.data_transformers.disable_max_rows()
+
+
+def form_df(samples, num_chains, rows):
+    dfs = []
+    for cn in range(num_chains):
+        for r in range(rows):
+            if rows>1:
+                df = pd.DataFrame(
+                        samples[:,cn, r]
+                )
+            else:
+                df = pd.DataFrame(
+                        samples[:,cn]
+                )
+            df.insert(0, 'idx', np.arange(df.shape[0]))
+            df = df.melt(id_vars ='idx', var_name = 'col')
+            df.insert(1, 'row' , r)
+            df.insert(1, 'chain', cn)
+            dfs.append(df)
+    return pd.concat(dfs).reset_index(drop=True)
+
+
+def get_post_df(samples):
+    num_chains = samples.shape[1]
+    if samples.ndim > 3:
+        rows = samples.shape[2]
+        df = form_df(samples, num_chains, rows)
+    else:
+        rows = 1
+        df = form_df(samples, num_chains, 1)
+    return df
+
+
+def plot_density(df, width=300, height=50) :
+    return alt.Chart(df).transform_fold(
+        ['value']
+    ).transform_density(
+        density='value',
+        groupby=['cn', 'row', 'col', 'source'],
+    ).mark_area(opacity=0.5).encode(
+        alt.X('value:Q'),
+        alt.Y('density:Q'),
+        alt.Row('row'),
+        alt.Column('col'),
+        alt.Color('source')
+    ).resolve_scale(
+        x='independent'
+    ).properties(width=width, height=height)
+
