cdd694132e11f831a4afd0990e7bcf2e11b863f1
konstantinos <kv@itemgmt.com>
2021-01-04 19:33:17 -0500

    added minor update

--- src/run_ibis_lvm.py
+++ src/run_ibis_lvm.py
@@ -17,24 +17,23 @@ def run_ibis_lvm(
     bundle_size,
     gen_model,
     log_dir,
-    degeneracy_limit = 0.5,
-    name = 'ibislvm'
-    ):
+    degeneracy_limit=0.5,
+    name="ibislvm",
+):
 
-
-    param_names = model_phonebook(model_num)['param_names']
-    latent_names = model_phonebook(model_num)['latent_names']
+    param_names = model_phonebook(model_num)["param_names"]
+    latent_names = model_phonebook(model_num)["latent_names"]
     jitter_corrs = dict()
     for p in param_names:
         jitter_corrs[p] = np.zeros(exp_data.size)
     particles = ParticlesLVM(
-        name = 'ibis_lvm',
-        model_num = model_num,
-        size = size,
+        name="ibis_lvm",
+        model_num=model_num,
+        size=size,
         bundle_size=bundle_size,
-        param_names = param_names,
-        latent_names = latent_names,
-        latent_model_num= 1
+        param_names=param_names,
+        latent_names=latent_names,
+        latent_model_num=1,
     )
     particles.set_log_dir(log_dir)
     if gen_model:
@@ -47,54 +46,56 @@ def run_ibis_lvm(
     log_lklhds = np.empty(exp_data.size)
     degeneracy_limit = 0.5
 
-
-    particles.sample_prior_particles(exp_data.get_stan_data()) # sample prior particles
-    particles.reset_weights() # set weights to 0
+    particles.sample_prior_particles(exp_data.get_stan_data())  # sample prior particles
+    particles.reset_weights()  # set weights to 0
     particles.initialize_bundles(exp_data.get_stan_data())
 
-    for t in tqdm(range(exp_data.size)):    
+    for t in tqdm(range(exp_data.size)):
         particles.sample_latent_bundle_at_t(t, exp_data.get_stan_data_at_t(t))
         particles.get_theta_incremental_weights_at_t(t, exp_data.get_stan_data_at_t(t))
-        log_lklhds[t] =  particles.get_loglikelihood_estimate()
-        
+        log_lklhds[t] = particles.get_loglikelihood_estimate()
+
         particles.update_weights()
 
-        if (essl(particles.weights) < degeneracy_limit * particles.size) and (t+1) < exp_data.size:
+        if (essl(particles.weights) < degeneracy_limit * particles.size) and (
+            t + 1
+        ) < exp_data.size:
             particles.resample_particles_bundles()
-            particles.jitter_bundles(exp_data.get_stan_data_upto_t(t+1))
-            
+            set_trace()
+            particles.jitter_bundles(exp_data.get_stan_data_upto_t(t + 1))
+
             ## add corr of param before jitter
             pre_jitter = dict()
             for p in param_names:
                 pre_jitter[p] = particles.particles[p].flatten()
             ####
 
-            particles.jitter(t+1, exp_data.get_stan_data_upto_t(t+1))
+            particles.jitter(t + 1, exp_data.get_stan_data_upto_t(t + 1))
 
             ## add corr of param
             for p in param_names:
-                jitter_corrs[p][t] = np.corrcoef(pre_jitter[p],particles.particles[p].flatten())[0,1]          
+                jitter_corrs[p][t] = np.corrcoef(
+                    pre_jitter[p], particles.particles[p].flatten()
+                )[0, 1]
             ####
 
             particles.reset_weights()
         else:
             pass
 
-        save_obj(t, 't', log_dir)
-        save_obj(particles, 'particles', log_dir)
-        save_obj(jitter_corrs, 'jitter_corrs', log_dir)
-        save_obj(log_lklhds, 'log_lklhds', log_dir)
+        save_obj(t, "t", log_dir)
+        save_obj(particles, "particles", log_dir)
+        save_obj(jitter_corrs, "jitter_corrs", log_dir)
+        save_obj(log_lklhds, "log_lklhds", log_dir)
 
-
-
-    print('\n\n')
+    print("\n\n")
     marg_lklhd = np.exp(logsumexp(log_lklhds))
-    print('Marginal Likelihood %.5f'%marg_lklhd)
-    save_obj(marg_lklhd, 'marg_lklhd', log_dir)
+    print("Marginal Likelihood %.5f" % marg_lklhd)
+    save_obj(marg_lklhd, "marg_lklhd", log_dir)
 
     output = dict()
-    output['particles'] = particles
-    output['log_lklhds'] = log_lklhds
-    output['marg_lklhd'] = marg_lklhd
-    output['jitter_corrs'] = jitter_corrs
+    output["particles"] = particles
+    output["log_lklhds"] = log_lklhds
+    output["marg_lklhd"] = marg_lklhd
+    output["jitter_corrs"] = jitter_corrs
     return output
