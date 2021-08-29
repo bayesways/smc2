b39e7f5bc0183c16ee3708731f04a9b14da3a932
Konstantinos Vamvourellis <>
2021-08-29 19:00:56 -0400

    minor

--- src/run_sim_smclvm.py
+++ src/run_sim_smclvm.py
@@ -8,20 +8,16 @@ from scipy.special import logsumexp
 from pdb import set_trace
 
 parser = argparse.ArgumentParser()
-parser.add_argument(
-    "-th", "--task_handle", help="hande for task", type=str, default="_"
-)
-parser.add_argument(
-    "-xdir",
-    "--existing_directory",
-    help="refit compiled model in existing directory",
-    type=str,
-    default=None,
-)
-parser.add_argument("-gm", "--gen_model", help="generate model", type=bool, default=0)
-parser.add_argument("-model", "--model_num", help="model number", type=int, default=0)
+parser.add_argument("-th", "--task_handle",
+                    help="hande for task", type=str, default="_")
+parser.add_argument("-xdir", "--existing_directory",
+                    help="refit compiled model in existing directory",
+                    type=str, default=None)
+parser.add_argument("-gm", "--gen_model",
+                    help="generate model", type=bool, default=0)
+parser.add_argument("-model", "--model_num",
+                    help="model number", type=int, default=0)
 args = parser.parse_args()
-
 
 if args.existing_directory is None:
     log_dir = make_folder(args.task_handle)
@@ -39,4 +35,10 @@ exp_data.generate()
 save_obj(exp_data, "data", log_dir)
 model_num = 1
 
-smclvm = run_smclvm(exp_data, model_num, 12, args.gen_model, log_dir)
+smclvm = run_smclvm(
+    exp_data,
+    args.model_num,
+    12,
+    args.gen_model,
+    log_dir
+    )
